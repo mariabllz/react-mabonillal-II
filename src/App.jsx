3bc04f4975002522c6f3a682b6e567c344425282
Maria <mabonillal@ufpso.edu.co>
2026-09-24 20:27:55 -0500

    subiendo cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import pollitoImg from './assets/pollito.jpg'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,28 +6,44 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
         <div>
-          <h1>Hola, soy María</h1>
+          <div className="presentacion">
+            <h1>Miloop Crochet</h1>
           <p>
-            Este es mi primer proyecto con React
+            El crochet es mi hobby favorito y también se convirtió en un pequeño emprendimiento.
           </p>
           <p>
-          Soy estudiante de ingeniería de sistemas de tercer semestre
-          y estoy aprendiendo a crear proyectos con React.
+          En Miloop Crochet encontrarás amigurumis, llaveros 
+          y otras pequeñas creaciones tejidas a mano.
           </p>
-          <img src={pollitoImg} alt="Imagen pollito crochet" />
-        </div>
+
+          <img src={pollitoImg} alt="Imagen pollito crochet" /></div>
+
+          <h2 className="titulo-creaciones">Lo que hago</h2>
+
+          <div className="creaciones">
+
+            <div>
+              <h3>Amigurumis</h3>
+            <p>Animalitos y personajes tejidos a mano</p>
+            </div>
+
+            <div>
+              <h3>Llaveros</h3>
+            <p>Pequeños detalles para llevar contigo</p>
+            </div>
+
+            <div>
+              <h3>Detalles Personalizados</h3>
+            <p>Creaciones especiales para regalar</p>
+            </div>
+            </div>
+            </div>
       </section>
+          
+          
 
-      <div className="ticks"></div>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    
     </>
   )
 }
